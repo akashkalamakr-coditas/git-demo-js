a4b26a53b62047c1ffdac9434aae43e57330a762
Akash Kalamkar <akash.kalamkar@coditas.com>
2026-05-07 10:20:08 +0530

    Add additional login test message

--- src/main/java/com/demo/jsgidemo/Login.java
+++ src/main/java/com/demo/jsgidemo/Login.java
@@ -3,5 +3,6 @@ package com.demo.jsgidemo;
 public class Login {
     void test(){
         System.out.println("this is login test");
+     System.out.println("this is login test for additonal login");
     }
 }
