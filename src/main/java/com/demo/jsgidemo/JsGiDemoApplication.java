c4d725133e92ce36f9cdc05aed0b1990cec2acbe
Akash Kalamkar <akash.kalamkar@coditas.com>
2026-05-07 09:44:33 +0530

    inital commit

---
+++ src/main/java/com/demo/jsgidemo/JsGiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jsgidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsGiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsGiDemoApplication.class, args);
+    }
+
+}
