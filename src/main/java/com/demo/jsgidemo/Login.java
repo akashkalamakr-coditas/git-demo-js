dbf6add7940be347efe4b6dbb91c93a334d2c25d
Akash Kalamkar <akash.kalamkar@coditas.com>
2026-05-07 10:21:09 +0530

    Add additional login test output to Login class

--- src/main/java/com/demo/jsgidemo/Login.java
+++ src/main/java/com/demo/jsgidemo/Login.java
@@ -4,5 +4,6 @@ public class Login {
     void test(){
         System.out.println("this is login test");
      System.out.println("this is login test for additonal login");
+             System.out.println("this is login test for additonal login 2");
     }
 }
