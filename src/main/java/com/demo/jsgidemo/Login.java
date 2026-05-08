9a46c8d0df8c4f8406db2f5cf83c571b3af6c862
Akash Kalamkar <akash.kalamkar@coditas.com>
2026-05-08 10:48:28 +0530

    second commit

--- src/main/java/com/demo/jsgidemo/Login.java
+++ src/main/java/com/demo/jsgidemo/Login.java
@@ -6,5 +6,9 @@ public class Login {
      System.out.println("this is login test for additonal login");
              System.out.println("this is login test for additonal login 2");
              System.out.println("this is login test for additonal login 2");
+             System.out.println("feature branch commit");
+
+        System.out.println("first commit ");
+        System.out.println("first commit ");
     }
 }
